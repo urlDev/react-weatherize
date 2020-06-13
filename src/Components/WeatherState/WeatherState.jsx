d83be02294bd9f800ab27da9fb30a0ae06a73b50
Can Ural <can.ural@outlook.com>
2020-06-13 21:30:18 +0300

    Fixed a bug for when the weather name is longer than some length, weather image was showing weirdly. Added a conditional there. Updated yarn.lock

--- src/Components/WeatherState/WeatherState.jsx
+++ src/Components/WeatherState/WeatherState.jsx
@@ -1,19 +1,36 @@
-import React, { Component } from "react";
-import { WeatherConsumer } from "../../Context";
-import "./WeatherState.scss";
+import React, { Component } from 'react';
+import { WeatherConsumer } from '../../Context';
+import './WeatherState.scss';
 
 class WeatherState extends Component {
   render() {
     return (
       <WeatherConsumer>
-        {value => {
+        {(value) => {
           return (
             <div className="weatherState">
-              <div className="weatherName">
+              <div
+                className={
+                  value.weatherName.length > 10
+                    ? 'weatherNameLong'
+                    : 'weatherName'
+                }
+              >
                 <h1>{value.weatherName}</h1>
               </div>
-              <div className="weatherImg">
-                <img src={`https://openweathermap.org/img/wn/${value.weatherImg}@2x.png`} alt={value.weatherName}></img>
+              <div
+                className="weatherImg"
+                style={
+                  value.weatherName.length > 10 ? { marginTop: '15px' } : null
+                }
+              >
+                <img
+                  src={
+                    value.weatherImg &&
+                    `https://openweathermap.org/img/wn/${value.weatherImg}@2x.png`
+                  }
+                  alt={value.weatherName}
+                ></img>
                 {/* <i className="fas fa-cloud-sun fa-4x"></i> */}
               </div>
             </div>
